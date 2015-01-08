28d11bb4e022c9eece0a47f539ef615c28ed173b
Oh Jinkyun <tintypemolly@gmail.com>
2015-01-09 02:46:25 +0900

    no redis session

--- helper/site.py
+++ helper/site.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
-import json
-
-import redis
 from twisted.web.server import Site, Session
 
-from config import REDIS_CONNECT_ARGS
 from helper.request import YuzukiRequest
-
-r = redis.StrictRedis(**REDIS_CONNECT_ARGS)
 
 
 class YuzukiSession(Session):
@@ -18,7 +12,7 @@ class YuzukiSession(Session):
         self.yuzuki_session_data = dict()
 
     def redis_sync(self):
-        r.setex(get_redis_id(self.uid), self.sessionTimeout, json.dumps(self.yuzuki_session_data))
+        pass
 
 
 class YuzukiSite(Site):
@@ -26,26 +20,3 @@ class YuzukiSite(Site):
         Site.__init__(self, resource, *args, **kwargs)
         self.requestFactory = YuzukiRequest
         self.sessionFactory = YuzukiSession
-
-    def makeSession(self):
-        uid = self._mkuid()
-        session = self.sessions[uid] = self.sessionFactory(self, uid)
-        r.setex(get_redis_id(uid), YuzukiSession.sessionTimeout, json.dumps(dict()))
-        return session
-
-    def getSession(self, uid):
-        redis_session = r.get(get_redis_id(uid))
-        if redis_session:
-            r.expire(get_redis_id(uid), YuzukiSession.sessionTimeout)
-            if uid in self.sessions:
-                session = self.sessions[uid]
-            else:
-                session = self.sessions[uid] = self.sessionFactory(self, uid)
-            session.yuzuki_session_data = json.loads(redis_session)
-            return session
-        else:
-            raise KeyError(uid)
-
-
-def get_redis_id(session_uid):
-    return "yzks::%s" % session_uid
